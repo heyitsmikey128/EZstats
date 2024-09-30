099d1be29a7f3631beb50f6f885a90365c16fd43
Revoker <Revoker.yt@gmail.com>
2024-09-29 23:24:11 -0400

    Added funtion
    -Added function to items that have more +healing than Spell damage
    -Changed display to include "Equip: " to be more inline with other affects

--- EZstats.lua
+++ EZstats.lua
@@ -8,17 +8,26 @@ local function ModifyTooltip(tooltip)
         if leftTextObject then
             local leftText = leftTextObject:GetText()
             local amount = string.match(leftText, "by up to (%d+)")
+            local damt = string.match(leftText, "damage done by up to (%d+)")
+            local hamt = string.match(leftText, "healing done by up to (%d+)")
+            
+            
             -- Check if the left text object contains the pattern you're looking for
-            if leftText and string.find(leftText, "Equip: Increases healing done") then
-                leftTextObject:SetText("+" .. amount .. " Healing Power\n")
+            if leftText and string.find(leftText, "Equip: Increases healing done by spells") then
+                leftTextObject:SetText("Equip: +" .. amount .. " Spell Healing\n")
+            end
+            
+            if leftText and string.find(leftText, "Equip: Increases healing done by up to") then
+                leftTextObject:SetText("Equip: +" .. hamt .. " Spell Healing\n" .. "Equip: +" .. damt .. " Spell Damage\n")
             end
             
             if leftText and string.find(leftText, "Equip: Increases damage and healing done") then
-                leftTextObject:SetText("+" .. amount .. " Spell Damage\n" .. "+" .. amount .. " Healing Power\n")
+                leftTextObject:SetText("Equip: +" .. amount .. " Spell Healing\n" .. "Equip: +" .. amount .. " Spell Damage\n")
             end
+
          
             if leftText and string.find(leftText, "Equip: Increases damage done by Shadow") then
-                leftTextObject:SetText("+" .. amount .. " Shadow Damage")
+                leftTextObject:SetText("Equip: +" .. amount .. " Shadow Damage")
             end
             
         end
