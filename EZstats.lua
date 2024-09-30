0ea9e855661662025ff882b45ecb248b51a2b9b4
heyitsmikey128 <michael.kushma@gmail.com>
2024-09-29 21:12:17 -0700

    Update EZstats.lua
    Added Crit and Hit to functions

--- EZstats.lua
+++ EZstats.lua
@@ -7,18 +7,35 @@ local function ModifyTooltip(tooltip)
         
         if leftTextObject then
             local leftText = leftTextObject:GetText()
-            local amount = string.match(leftText, "by up to (%d+)")
+            local damage = string.match(leftText, "damage done by up to (%d+)")
+            local healing = string.match(leftText, "healing done by up to (%d+)")
+            local hit = string.match(leftText, "critical strike with all spells and attacks by (%d+)")
+            local crit = string.match(leftText, "chance to hit with all spells and attacks by (%d+)")
+            
+            
             -- Check if the left text object contains the pattern you're looking for
-            if leftText and string.find(leftText, "Equip: Increases healing done") then
-                leftTextObject:SetText("+" .. amount .. " Healing Power\n")
+            if leftText and string.find(leftText, "Equip: Increases healing done by spells") then
+                leftTextObject:SetText("Equip: +" .. healing .. " Spell Healing\n")
+            end
+            
+            if leftText and string.find(leftText, "Equip: Increases healing done by up to") then
+                leftTextObject:SetText("Equip: +" .. healing .. " Spell Healing\n" .. "Equip: +" .. damage .. " Spell Damage\n")
             end
             
             if leftText and string.find(leftText, "Equip: Increases damage and healing done") then
-                leftTextObject:SetText("+" .. amount .. " Spell Damage\n" .. "+" .. amount .. " Healing Power\n")
+                leftTextObject:SetText("Equip: +" .. healing .. " Spell Healing\n" .. "Equip: +" .. damage .. " Spell Damage\n")
             end
          
             if leftText and string.find(leftText, "Equip: Increases damage done by Shadow") then
-                leftTextObject:SetText("+" .. amount .. " Shadow Damage")
+                leftTextObject:SetText("Equip: +" .. damage .. " Shadow Damage")
+            end
+
+            if leftText and string.find(leftText, "Equip: Improves your chance to hit") then
+                leftTextObject:SetText("Equip: " .. hit .. " % Hit")
+            end
+            
+            if leftText and string.find(leftText, "Equip: Improves your chance to get a critical") then
+                leftTextObject:SetText("Equip: " .. crit .. " % Crit")
             end
             
         end
